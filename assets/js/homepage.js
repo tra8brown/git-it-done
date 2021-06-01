9431316a9adbae0fde02c66635e22411a9f5c274
TraBee <tra8brown@gmail.com>
2021-06-01 11:12:47 -0600

    updated with some code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+        //fetch("https://api.github.com/users/octocat/repos"); //made an http request
+        fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+            console.log("inside", response);
+        });
 
-getUserRepos();
+        console.log("outside")
