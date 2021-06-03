b780d85a7de6e3623753c07acd3813c469527990
TraBee <tra8brown@gmail.com>
2021-06-02 18:39:21 -0600

    updated

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +13,18 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos("microsoft");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
